3a6e7f67df487db75efe76555dd06988a5631bfb
Yasmin <jasminvk@yandex.ru>
2022-08-05 16:11:47 +0300

    Task4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введи 2 числа и мы тебе скажем какое из них больше");
+ int a, b;
+ Console.WriteLine("Введи свое первое число - ");
+ a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введи свое второе число - ");
+ b = Convert.ToInt32(Console.ReadLine());
+ 
+bool Chto0 = a > b;
+bool Chto1 = a == b;
+bool Chto2 = a < b;
+ 
+if (Chto0)
+{
+ Console.WriteLine($"Число {a} больше чем {b}");
+}
+else if (Chto1)
+{
+Console.WriteLine($"Числа {a} и {b} равны");
+}
+else
+{
+Console.WriteLine($"Число {b} больше чем {a}");
+}
